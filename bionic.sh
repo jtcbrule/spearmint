b08dc4b89d58dbdd6da11d6c58c6019e18b74fa1
jtcb <jbrule@cs.umd.edu>
2018-07-22 18:26:55 -0400

    Update bionic.sh

--- bionic.sh
+++ bionic.sh
@@ -6,12 +6,47 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 # base system
-apt install slick-greeter
+apt-get update
+apt-get -y install slick-greeter cinnamon-core
 
-apt install cinnamon-core
-rm /etc/netplan/01-netcfg.yaml
+# hack to fix network management
+mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml-old
+mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf-old
+touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
 
-apt install gnome-terminal
+# miscellaneous utilities
+apt-get -y install xdg-user-dirs-gtk
+apt-get -y install gnome-screenshot
+apt-get -y install lightdm-settings
 
-# utils
+# fonts
+apt-get -y install fonts-liberation fonts-noto
+
+# sounds
+apt-get -y sound-theme-freedesktop
+
+# printer support
+apt-get -y install cups system-config-printer
+
+# pdf viewer
+apt-get -y install atril
+
+# image viewer
+apt-get -y install eom
+
+# text editor
+apt-get -y install pluma
+
+# media player
+apt-get -y install vlc
+
+# web browser
+apt-get -y install firefox
+
+# webcam
+apt-get -y cheese
+
+# theme
+apt-get -y install dmz-cursor-theme
+gsettings set org.cinnamon.desktop.interface cursor-theme "DMZ-White"
 
