5784b4a880102cf777ba9e52b305ab6f39c2e23d
jtcb <jbrule@cs.umd.edu>
2018-12-28 17:55:11 -0500

    Cleanup/simplify bionic.sh

--- README.md
+++ README.md
@@ -1,22 +1,12 @@
 # Spearmint
 
-Mint-like distribution. Guidelines:
-
-- Depends only on the contents of this git repository and Ubuntu LTS packages
-    - May consider creating versions for other Linux distributions (e.g. Debian)
-- Cinnamon desktop environment
-- Avoid apps with GNOME-style client-side decorations
-- Lightweight, easily customizable
+Unofficial Ubuntu flavor using the Cinnamon desktop environment
 
 ## Installation
 
-- Network install Ubuntu 18.04 LTS https://www.ubuntu.com/download/alternative-downloads
+- Network install Ubuntu 18.04 LTS: https://www.ubuntu.com/download/alternative-downloads#network-installer
 - `wget https://raw.githubusercontent.com/jtcbrule/spearmint/master/bionic.sh`
 - Review the contents of `bionic.sh`
 - `sudo bash bionic.sh`
-- Reboot (`sudo shutdown -r now`)
-
-## TODO
-
-Organize the `bionic.sh` script
+- Reboot (`shutdown -r now`)
 
--- bionic.sh
+++ bionic.sh
@@ -6,76 +6,19 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 apt-get update
-
-# desktop environment
-apt-get -y install slick-greeter cinnamon-core
+apt-get -y install slick-greeter cinnamon-desktop-environment
 
 # hack to fix network management
 mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml-old
-mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf-old
+mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf \
+   /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf-old
 touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
 
-# gtk bookmark file
-apt-get -y install xdg-user-dirs-gtk
+apt-get -y install dmz-cursor-theme arc-theme moka-icon-theme
 
-# font viewer
-apt-get -y install gnome-font-viewer
-
-# login window settings
-apt-get -y install lightdm-settings
-
-# fonts
-apt-get -y install fonts-liberation fonts-noto
-
-# sounds
-apt-get -y install sound-theme-freedesktop
-
-# screenshot utility
-apt-get -y install gnome-screenshot
-
-# system monitor
-apt-get -y install gnome-system-monitor
-
-# dialog boxes from shell scripts
-apt-get -y install zenity
-
-# printer support
-apt-get -y install cups system-config-printer
-
-# pdf viewer
-apt-get -y install atril
-
-# image viewer
-apt-get -y install eom
-
-# text editor
-apt-get -y install pluma
-
-# media player
-apt-get -y install vlc
-
-# web browser
-apt-get -y install firefox
-
-# webcam
-apt-get -y install cheese
-
-# cursor
-apt-get -y install dmz-cursor-theme
-# TODO set cursor from command line
-
-# command line utilities
-apt-get -y install git
-apt-get -y install apt-listchanges
-
-# mint-y theme
-git clone https://github.com/jtcbrule/spearmint.git
-cp -r spearmint/icons/. /usr/share/icons
-cp -r spearmint/themes/. /usr/share/themes
-# TODO set theme and icons from command line
-
-# cleanup
-if [ -f spearmint/bionic.sh ]; then
-    rm $0
-fi
+gsettings set org.cinnamon.desktop.wm.preferences theme 'Arc'
+gsettings set org.cinnamon.desktop.interface icon-theme 'Moka'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Arc'
+gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-White'
+gsettings set org.cinnamon.theme name 'Arc'
 
