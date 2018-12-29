c1f75293bd7f1f408f458d2e9d5d38d285dfb1cb
jtcb <jbrule@cs.umd.edu>
2018-12-28 19:02:03 -0500

    Fix bug with gsettings

--- bionic.sh
+++ bionic.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-if ! [ $(id -u) = 0 ]; then
-    echo "Root privileges are required."
-    exit 1
-fi
-
-apt-get update
+sudo apt-get update
 
 # base
-apt-get -y install slick-greeter cinnamon-desktop-environment
+sudo apt-get -y install slick-greeter cinnamon-desktop-environment
+
+# gui apt updates
+sudo apt-get -y install update-manager
 
 # hack to fix network management
-mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml-old
-mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf \
+sudo mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml-old
+sudo mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf \
    /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf-old
-touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
+sudo touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
 
 # themes and icons
-apt-get -y install dmz-cursor-theme arc-theme moka-icon-theme
+sudo apt-get -y install dmz-cursor-theme arc-theme moka-icon-theme
 
 gsettings set org.cinnamon.desktop.wm.preferences theme 'Arc'
 gsettings set org.cinnamon.desktop.interface icon-theme 'Moka'
@@ -25,6 +23,8 @@ gsettings set org.cinnamon.desktop.interface gtk-theme 'Arc'
 gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-White'
 gsettings set org.cinnamon.theme name 'Arc'
 
-# gui apt updates
-apt-get -y install update-manager
+# change volume sound effect
+gsettings set org.cinnamon.desktop.sound volume-sound-enabled true
+gsettings set org.cinnamon.desktop.sound volume-sound-file \
+    '/usr/share/sounds/freedesktop/stereo/dialog-information.oga'
 
